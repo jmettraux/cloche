54d7f5cf933ce2a7cc502b8eb646bf111615db96
John Mettraux <jmettraux@gmail.com>
2010-01-11 17:10:57 +0900

    helpers

--- src/cloche_http.erl
+++ src/cloche_http.erl
@@ -7,28 +7,38 @@ start(Name, Port, Cloche) ->
   mochiweb_http:start([
     { name, Name },
     { port, Port },
-    { loop, fun(Req) -> handle(Req:get(method), Req, Cloche) end } ]).
+    { loop, fun(Req) -> handle(Req, Cloche) end } ]).
 
 stop(Name) ->
   mochiweb_http:stop(Name).
 
 %handle(Req) ->
-%  handle(Req:get(method), Req).
+  %Req:serve_file("toto.json", ".").
+  %handle(Req:get(method), Req).
   %io:format("===== ~p~n", [ Req:get(method) ]),
   %Req:ok({ "text/html", "toto" }).
 
+handle(Req, Cloche) ->
+  handle(Req:get(method), Req, Cloche).
+
 handle('GET', Req, Cloche) ->
-  %Req:ok({ "text/html", "GOT it" }).
-  %Req:serve_file("toto.json", ".").
+  %io:format("GET ~p~n", [ Req ]),
   case re:split(Req:get(path), "\/", [ { return, list } ]) of
     [ [], Type, Id | _ ] ->
       case cloche:do_get(Cloche, Type, Id) of
-        undefined ->
-          Req:respond({ 404, [{ "Content-Type", "text/plain" }], "not found." });
-        Json ->
-          Req:ok({ "application/json", Json })
+        undefined -> not_found(Req);
+        Json -> Req:ok({ "application/json", Json })
       end;
-    Any ->
-      Req:ok({ "text/plain", "I don't get it : " ++ Any })
+    Any -> error(Req, "I don't get it : " ++ Any)
   end.
+
+%
+% helpers
+%
+
+not_found(Req) ->
+  Req:respond({ 404, [{ "Content-Type", "text/plain" }], "not found." }).
+
+error(Req, Msg) ->
+  Req:respond({ 500, [{ "Content-Type", "text/plain" }], "error : " ++ Msg }).
 
